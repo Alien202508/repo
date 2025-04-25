c49ae3b84280e74985c3b1fec96924b4770c6cc9
Alien202508 <alien.x.3103@gmail.com>
2025-04-25 19:59:43 +0100

    Update relay_server.py

--- relay_server.py
+++ relay_server.py
@@ -1,36 +1,40 @@
 from flask import Flask, request, jsonify
-import os
 
 app = Flask(__name__)
-client_to_server = []
+
+# تخزين الرسائل
 server_to_client = []
+client_to_server = []
 
-@app.route('/send_to_server', methods=['POST'])
-def send_to_server():
-    data = request.json
-    client_to_server.append(data)
-    return jsonify({"status": "received"})
-
-@app.route('/get_from_client', methods=['GET'])
-def get_from_client():
-    if client_to_server:
-        return jsonify(client_to_server.pop(0))
-    else:
-        return jsonify({})
+@app.route('/')
+def index():
+    return 'Relay Server is Running'
 
 @app.route('/send_to_client', methods=['POST'])
 def send_to_client():
     data = request.json
     server_to_client.append(data)
-    return jsonify({"status": "sent"})
+    return jsonify({"status": "Command received"})
 
-@app.route('/get_from_server', methods=['GET'])
-def get_from_server():
+@app.route('/get_from_client', methods=['GET'])
+def get_from_client():
     if server_to_client:
         return jsonify(server_to_client.pop(0))
     else:
         return jsonify({})
 
+@app.route('/send_to_server', methods=['POST'])
+def send_to_server():
+    data = request.json
+    client_to_server.append(data)
+    return jsonify({"status": "Output received"})
+
+@app.route('/get_from_server', methods=['GET'])
+def get_from_server():
+    if client_to_server:
+        return jsonify(client_to_server.pop(0))
+    else:
+        return jsonify({})
+    
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=10000)
